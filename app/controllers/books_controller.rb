a7ee4fa6b45bbf6ab39e42d8dd05727f96a10957
themuhaimin@gmail.com <themuhaimin@gmail.com>
2018-01-26 09:21:23 +0700

    crud function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,34 @@
 class BooksController < ApplicationController
+  def index
+      @books=Book.all
+  end
   def show
+        @books=Book.find(params[:id])
+  end
+  def new
     @book=Book.new
+  end
+  def edit
+    @book=Book.find(params[:id])
+  end
+  def update
+    @book=Book.find(params[:id])
+    @book.update(resources_params)
+    render plain: "Berhasil mengubah buku baru"
+  end
+  def create
+  #  render plain: params[:book]
+  book = Book.new(resources_params)
+  book.save
+  render plain: "Berhasil Menyimpan"
+  end
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    render plain: "Berhasil Menghapus Buku"
+  end
+  private
+  def resources_params
+    params.require(:book).permit(:title,:page,:description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 get 'home/index'
 root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :books
+  #get '/books', to:'books#index'
+  resources :books
 end
